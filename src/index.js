cae5f0bbdb4278e79f594d66127ab6153f0d95b1
mikomokaru <hisato@tanakake.band>
2019-11-09 17:27:05 +0900

    first-commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
+const $ = require('jquery')
+const _ = require('lodash')
+const Bacon = require('baconjs')
+
+console.log("Hello World from your main file!");
+
